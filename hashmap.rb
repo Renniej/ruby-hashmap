8503f62fa80192c540b2acdf4f4ca4767e29561f
renniej <17017622+Renniej@users.noreply.github.com>
2025-05-11 07:11:09 -0400

    implement hashmap

--- hashmap.rb
+++ hashmap.rb
@@ -1,41 +1,115 @@
-=begin
-Hash {
+require_relative "linked_list"
 
-  create property of type number called load_factor with the initial value of 0.75
-  create property of type number called capacity with the initial value of 16
-  create property of type array<LinkedList> called buckets with the initial value of empty nil list
+class HashMap
 
-  hash(key : String)  
-    create variable of type number called hash_code with the initial value of 0
-    create variable of type number called prime_number with the initial value of 31
-    for each character in key
-        set hash_code to prime_number * hash_code + ( result of convert char to ascii value )
-    end
+  def initialize() {
+    @numOfEntries = 0
+    @load_factor = 0.75
+    @capacity = 16
+    @buckets = Array.new(@capacity) { LinkedList.new }
+  }
+
+  def hash(key)
+    hash_code = 0
+    prime_number = 31
+    key.each_char { |char| hash_code = prime_number * hash_code + char.ord }
+    hash_code
   end
 
-
-  set(key, value) 
-    create a variable of type number called index with the initial value of hash(key) % capacity
-    create a variable called bucket with the initial value of buckets[index]
-    
-    if bucket is empty then
-      set buckets[index] to a new node(key, value)
+  def set(key,value)
+    bucket = getBucket(key)
+    node = bucket.findNode? {| node | node[:value][:key] == key}
+    if (!node.nil?) 
+       node[:value] = value
     else
-      for each node in bucket
-        create a variable called entry with the initial value of node's value property
-        if entry's key is equal to key then 
-          set the value to value and return the function
-        end
-      end
-      append a new node(key,value) to the end of the linkedlist 
+      bucket.append({
+        key: key,
+        value: value, 
+      })
+      incrementEntries()
     end
   end
 
-  get(key)
-    
+  def get(key) 
+    bucket = getBucket(key)
+    bucket.findNode? {| node | node[:value][:key] == key}
+  end
+
+  def has?(key) 
+    (bucket.findNode? {| node | node[:value][:key] == key}).nil?
+  end
+
+ 
+  def remove(key)
+    bucket = getBucket(key)
+    node_index = bucket.find {| node | node[:value][:key] == key}
+    node = bucket.at(nodeIndex)
+    prev_node = nodeIndex != 0 ? bucket.at(nodeIndex - 1) : nil
+    node.removeSelf(prev_node)
+    decrementEntries()
+  end
+
+  def keys
+    keys = []
+    @buckets.each do
+      {|bucket| bucket.each {|node| keys.push(node[:value][:key]) } }
+    end
+    keys
+  end
 
 
-}  
+  def values
+    values = []
+    @buckets.each do |bucket| 
+      bucket.each {|node| values.push(node[:value][:value]) } 
+    end
+    values
+  end
 
 
-=end
+  def entries 
+    pairs = []
+    @buckets.each do |bucket| 
+      bucket.each do |node| 
+        key = node[:value][:key]
+        value = node[:value][:value]
+        pairs.push([key,value])
+      end
+    end
+    pairs
+  end
+
+  def length 
+    @numOfEntries
+  end
+ 
+  def clear 
+    @buckets.each {|bucket| bucket.clear}
+  end
+
+
+private:
+
+ 
+  def getBucket(key) 
+    index = hash(key) % @capacity
+    raise IndexError if index.negative? || index >= @buckets.length
+    @buckets[index]
+  end
+
+  incrementEntries() {
+    @numOfEntries += 1
+    resize()
+  }
+
+  decrementEntries() {
+    @numOfEntries -= 1
+    resize()
+  }
+
+  resize() {
+    if @numOfEntries < (@capacity * @load_factor) return;
+
+  }
+
+end
